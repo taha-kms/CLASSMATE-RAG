516951d2d7be9ae33a1865cc48216946626a6c4b
TAHA KAMALISADEGHIAN <tahakamali14@gmail.com>
2025-08-26 21:49:27 +0200

    chore(retrieval): export rrf_fuse and HybridRetriever from retrieval package

--- rag/retrieval/__init__.py
+++ rag/retrieval/__init__.py
@@ -1,4 +1,11 @@
 from .vector_chroma import ChromaVectorStore, build_where_filter
 from .bm25 import BM25Store
+from .fusion import rrf_fuse, HybridRetriever
 
-__all__ = ["ChromaVectorStore", "build_where_filter", "BM25Store"]
+__all__ = [
+    "ChromaVectorStore",
+    "build_where_filter",
+    "BM25Store",
+    "rrf_fuse",
+    "HybridRetriever",
+]
